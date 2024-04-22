8615eefd9cc8da032d0a0902f3111f5b6fddf4f7
Vladislav Yaremenko <vlad.yaremenko.98@yandex.ru>
2024-04-22 21:03:18 +0300

    Add main func.

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from input import input_number
 from sequence import get_sequence
 
 
-if '__main__' == __name__:
+def main() -> str:
     arg_parser = parser_command_line()
     args = arg_parser.parse_args()
     if args.count:
-        sequence = get_sequence(args.count)
-    else:
-        sequence = get_sequence(input_number())
-    print(sequence)
+        return get_sequence(args.count)
+    return get_sequence(input_number())
+
+
+if '__main__' == __name__:
+    print(main())
